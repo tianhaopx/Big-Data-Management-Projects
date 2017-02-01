04882bc2aeca778432829d04deb2a5b8c9d2ca65
GuoTianhao <rggth09@gmail.com>
2017-02-01 18:50:46 -0500

    edit query2

--- Project1/src/query/Query2.java
+++ Project1/src/query/Query2.java
@@ -11,39 +11,44 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 import java.io.IOException;
 
 
 public class Query2 {
     public static class TransactionMapper extends Mapper<LongWritable, Text, Text, FloatWritable> {
-        private FloatWritable result = new FloatWritable();
+
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String[] str_value = value.toString().split(",");
-            result.set(Float.valueOf(str_value[2]));
-            context.write(new Text(str_value[1]),result);
+            context.write(new Text(str_value[1]), new FloatWritable(Float.valueOf(str_value[2])));
         }
     }
 
-    public static class SumTransactionReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
-        private FloatWritable result = new FloatWritable();
-        public void reduce(Text key, Iterable<FloatWritable> value, Context context) throws IOException, InterruptedException {
+    public static class SumTransactionReducer extends Reducer<Text, FloatWritable, Text, Text> {
+
+        public void reduce(Text key, Iterable<FloatWritable> value, Context context) throws IOException,
+                InterruptedException {
+            int numOfTrans = 0;
             float sum = 0.0f;
-            for (FloatWritable val:value) {
+            for (FloatWritable val : value) {
                 sum += val.get();
+                numOfTrans++;
             }
-            result.set(sum);
-            context.write(key,result);
+            context.write(key, new Text(numOfTrans + "," + sum));
         }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        conf.set("mapred.textoutputformat.separator", ",");
         Job job = Job.getInstance(conf, "Query2");
         job.setJarByClass(Query2.class);
         job.setMapperClass(Query2.TransactionMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(FloatWritable.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(FloatWritable.class);
-        job.setCombinerClass(Query2.SumTransactionReducer.class);
+        job.setOutputValueClass(Text.class);
+//        job.setCombinerClass(Query2.SumTransactionReducer.class);
         job.setReducerClass(Query2.SumTransactionReducer.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
