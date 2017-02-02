29b24854cb769994f34176b5418e457707ff3d4c
YouthBread <YouthBread@users.noreply.github.com>
2017-02-02 12:13:51 -0500

    Query2 with combiner

--- Project1/src/query/Query2.java
+++ Project1/src/query/Query2.java
@@ -2,8 +2,6 @@ package query;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -11,30 +9,28 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 import java.io.IOException;
 
 
 public class Query2 {
-    public static class TransactionMapper extends Mapper<LongWritable, Text, Text, FloatWritable> {
+    public static class TransactionMapper extends Mapper<LongWritable, Text, Text, Text> {
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String[] str_value = value.toString().split(",");
-            context.write(new Text(str_value[1]), new FloatWritable(Float.valueOf(str_value[2])));
+            context.write(new Text(str_value[1]), new Text("1"+","+Float.valueOf(str_value[2])));
         }
     }
 
-    public static class SumTransactionReducer extends Reducer<Text, FloatWritable, Text, Text> {
-
-        public void reduce(Text key, Iterable<FloatWritable> value, Context context) throws IOException,
-                InterruptedException {
+    public static class SumTransactionReducer extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> value, Context context) throws IOException, InterruptedException {
             int numOfTrans = 0;
             float sum = 0.0f;
-            for (FloatWritable val : value) {
-                sum += val.get();
-                numOfTrans++;
+            for (Text str:value) {
+                String[] str_value = str.toString().split(",");
+                sum += Float.valueOf(str_value[1]);
+                numOfTrans += Integer.valueOf(str_value[0]);
             }
-            context.write(key, new Text(numOfTrans + "," + sum));
+            context.write(key, new Text(Integer.toString(numOfTrans)+","+Float.toString(sum)));
         }
     }
 
@@ -45,11 +41,11 @@ public class Query2 {
         job.setJarByClass(Query2.class);
         job.setMapperClass(Query2.TransactionMapper.class);
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(FloatWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setCombinerClass(Query2.SumTransactionReducer.class);
+        job.setReducerClass(Query2.SumTransactionReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-//        job.setCombinerClass(Query2.SumTransactionReducer.class);
-        job.setReducerClass(Query2.SumTransactionReducer.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
