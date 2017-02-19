46e5b101b0e985fa7392035f48fed36628a67e1b
YouthBread <YouthBread@users.noreply.github.com>
2017-02-19 15:54:18 -0500

    Problem 2 inital

---
+++ Project2/src/Problem2/CustomInput.java
@@ -0,0 +1,17 @@
+package Problem2;
+
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+import java.io.IOException;
+
+/**
+ * Created by test on 2/19/17.
+ */
+public class CustomInput {
+    public static class CustomInputFormat extends InputFormat<LongWritable, Text> {
+
+    }
+}
