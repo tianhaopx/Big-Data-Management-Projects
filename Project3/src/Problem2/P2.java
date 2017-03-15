bb27f2ef920a2bc3e217f2171d0b658f04e73540
YouthBread <YouthBread@users.noreply.github.com>
2017-03-15 10:45:40 -0400

    scalable version

--- Project3/src/Problem2/P2.java
+++ Project3/src/Problem2/P2.java
@@ -6,69 +6,23 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
 import scala.Tuple2;
 
-import java.util.Arrays;
-import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 /**
  * Created by test on 3/8/17.
  */
 public class P2 {
-    public static Double calDensity(Tuple2 s, Map dic) {
-        int cell = (int)s._1;
+    public static Double calDensity(Tuple2 s) {
         double ans;
-        //from now we find the neighbors
-
-        //if the cell in the top left
-        if (cell == 1) {
-            // 3 neighbor 2,501,502
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(2)+(int)dic.get(501)+(int)dic.get(502));
-            return ans;
-        }
-        //if the cell in the bottom left
-        else if (cell == 249501) {
-            // 3 neighbor 249000,249001,249501
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(249000)+(int)dic.get(249001)+(int)dic.get(249501));
-            return ans;
-        }
-        // if the cell in the top right
-        else if (cell == 500) {
-            // 3 neighbor 499,999,1000
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(499)+(int)dic.get(999)+(int)dic.get(1000));
-            return ans;
-        }
-        // if the cell in the bottom right
-        else if (cell == 250000) {
-            // 3 neighbor 249499,249500,249999
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(249499)+(int)dic.get(249500)+(int)dic.get(249999));
-            return ans;
-        }
-        // if the cell on the top edge
-        else if ((cell>1 && cell <500)) {
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(cell-1)+(int)dic.get(cell+1)+(int)dic.get(cell+499)+(int)dic.get(cell+500)+(int)dic.get(cell-1)+(int)dic.get(cell+501));
-            return ans;
-        }
-        // if the cell on the bottom edge
-        else if (cell>249501 && cell<250000) {
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(cell-1)+(int)dic.get(cell+1)+(int)dic.get(cell-499)+(int)dic.get(cell-500)+(int)dic.get(cell-1)+(int)dic.get(cell-501));
-            return ans;
-        }
-        // if the cell on the left edge
-        else if (cell%500==1 && cell != 1 && cell != 249501){
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(cell-500)+(int)dic.get(cell-499)+(int)dic.get(cell+1)+(int)dic.get(cell+500)+(int)dic.get(cell+501));
-            return ans;
-        }
-        // if the cell on the right edge
-        else if (cell%500==0 && cell != 500 && cell != 250000){
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(cell-500)+(int)dic.get(cell-501)+(int)dic.get(cell-1)+(int)dic.get(cell+500)+(int)dic.get(cell+499));
-            return ans;
-        }
-        // the cell in the middle
-        else {
-            ans = Double.valueOf(dic.get(cell).toString())/((int)dic.get(cell-1)+(int)dic.get(cell+1)+(int)dic.get(cell-501)+(int)dic.get(cell-500)+(int)dic.get(cell-499)+(int)dic.get(cell+499)+(int)dic.get(cell+500)+(int)dic.get(cell+501));
-            return ans;
-        }
+        String[] temp = s._2.toString().split(",");
+        ans = Double.valueOf(temp[0])/Double.valueOf(temp[1]);
+        return ans;
     }
 
     public static void main(String[] args) {
@@ -76,23 +30,138 @@ public class P2 {
             public String[] call(String s) { return s.split(","); }
         }
 
+        class AssignCell implements PairFlatMapFunction<String[], Integer, String> {
+            public Iterator<Tuple2<Integer,String>> call(String[] s){
+                ArrayList<Tuple2<Integer,String>> list = new ArrayList<Tuple2<Integer,String>>();
+                int cell = (int)(Math.floor(Integer.valueOf(s[0])/20)+Math.floor(Math.abs(Integer.valueOf(s[1])-9999)/20)*500+1);
+
+                //if the cell in the top left
+                if (cell == 1) {
+                    // 3 neighbor 2,501,502
+                    for (int i:new int[] {1,2,501,502}){
+                        if (i==1){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+                //if the cell in the bottom left
+                else if (cell == 249501) {
+                    // 3 neighbor 249000,249001,249501
+                    for (int i:new int[] {249501,249000,249001,249501}){
+                        if (i==249501){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+                // if the cell in the top right
+                else if (cell == 500) {
+                    // 3 neighbor 499,999,1000
+                    for (int i:new int[] {500,499,999,1000}){
+                        if (i==500){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+                // if the cell in the bottom right
+                else if (cell == 250000) {
+                    // 3 neighbor 249499,249500,249999
+                    for (int i:new int[] {250000,249499,249500,249999}){
+                        if (i==250000){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+                // if the cell on the top edge
+                else if ((cell>1 && cell <500)) {
+                    for (int i:new int[] {cell,cell-1,cell+1,cell+499,cell+500,cell-1,cell+501}){
+                        if (i==cell){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+                // if the cell on the bottom edge
+                else if (cell>249501 && cell<250000) {
+                    for (int i:new int[] {cell,cell-1,cell+1,cell-499,cell-500,cell-1,cell-501}){
+                        if (i==cell){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+                // if the cell on the left edge
+                else if (cell%500==1 && cell != 1 && cell != 249501){
+                    for (int i:new int[] {cell,cell-500,cell-499,cell+1,cell+500,cell+501}){
+                        if (i==cell){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+                // if the cell on the right edge
+                else if (cell%500==0 && cell != 500 && cell != 250000){
+                    for (int i:new int[] {cell,cell-500,cell-501,cell-1,cell+500,cell+499}){
+                        if (i==cell){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+                // the cell in the middle
+                else {
+                    for (int i:new int[] {cell,cell-1,cell+1,cell-501,cell-500,cell-499,cell+501,cell+500,cell+499}){
+                        if (i==cell){
+                            list.add(new Tuple2<Integer,String>(i,"1"+",1"));
+                        } else {
+                            list.add(new Tuple2<Integer,String>(i,"0"+",1"));
+                        }
+                    }
+                    return list.iterator();
+                }
+            }
+        }
+
+        class SumCell implements Function2<String,String,String> {
+            public String call(String a, String b) {
+                String[] temp1 = a.split(",");
+                String[] temp2 = b.split(",");
+                int self_cell =  Integer.valueOf(temp1[0])+Integer.valueOf(temp2[0]);
+                int relative_cell = Integer.valueOf(temp1[1])+Integer.valueOf(temp2[1]);
+                return self_cell+","+relative_cell;
+            }
+        }
 
         // calculate the nodes in each cube
         SparkConf conf = new SparkConf().setAppName("P2").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String[]> coorFile = sc.textFile(args[0]).map(new GetSplit());
-        JavaPairRDD<Integer, Integer> coordinates = coorFile
-                .mapToPair(coor -> new Tuple2<>((int)(Math.floor(Integer.valueOf(coor[0])/20)+Math.floor(Math.abs(Integer.valueOf(coor[1])-9999)/20)*500+1), 1))
-                .reduceByKey((a,b)->a+b);
+        JavaPairRDD<Integer, String> coordinates = coorFile
+                .flatMapToPair(new AssignCell())
+                .reduceByKey(new SumCell());
 
-        //find the nearby
-        Map temp = coordinates.collectAsMap();
-        // calculate the density
-//        JavaRDD<String> results = coordinates
-//                .map(new GetNeighbor(temp));
-
+        // better one
         JavaPairRDD<Double, Integer> density = coordinates
-                .mapToPair(coor -> new Tuple2<>(calDensity(coor,temp),coor._1))
+                .mapToPair(coor -> new Tuple2<>(calDensity(coor),coor._1))
                 .sortByKey(false);
 
         density.saveAsTextFile(args[1]);
